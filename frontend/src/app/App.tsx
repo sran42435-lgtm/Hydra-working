907a8a951ae4e9cadeda7adb6fed2f47c3b61948
Sran567iki <sran42435@gmail.com>
2026-04-28 20:30:40 +0800

    Update App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -5,7 +5,7 @@
  */
 
 import React from "react";
-import { ThemeProvider } from "./ThemeProvider";
+import ThemeProvider from "./ThemeProvider";
 import { AppRoutes } from "./AppRoutes";
 
 export const App: React.FC = () => {
